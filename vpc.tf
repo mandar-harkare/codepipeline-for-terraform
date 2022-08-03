060a666835da7fbeeeeb9e7020f1531be06a03b8
Mandar Harkare <mandar.harkare@edfenergy.com>
2022-08-03 17:38:40 +0530

    deploy vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
-# resource "aws_vpc" "default" {
-#   cidr_block = "1.2.0.0/16"
-#   tags = {
-#     Name = "codepipeline-vpc-2"
-#     Purpose = "codepipeline Demo again"
-#   }
-# }
+resource "aws_vpc" "default" {
+  cidr_block = "1.2.0.0/16"
+  tags = {
+    Name = "codepipeline-vpc-2"
+    Purpose = "codepipeline Demo again"
+  }
+}
